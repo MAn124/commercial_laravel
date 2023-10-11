092ee040de47ff003f3f5519202dffc3a66bdd83
An Thai <thaitranminhan@gmail.com>
2023-10-11 23:38:50 +0700

    Update create.blade.php

--- resources/views/admin/brand/create.blade.php
+++ resources/views/admin/brand/create.blade.php
@@ -1,9 +1,14 @@
 @extends('layout.master')
 @section('content')
 <form action="">
-    <div class="form-group">
+    <div class="form-group" class="brand-name">
         <label for="inputAddress2">Brand Name</label>
         <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+    </div>
+    <div id="editor" class="brand-desc">
+        <p>Hello World!</p>
+        <p>Some initial <strong>bold</strong> text</p>
+        <p><br></p>
     </div>
     <button class="btn btn-primary m-r-5" type="submit">Submit</button>
 </form>
